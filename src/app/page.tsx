870d34838fec9a6f92545216aa38a336bbb9c4be
Ali Zare <A.zare@charisma.tech>
2024-10-05 14:16:51 +0330

    fix(home page): add site link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="flex w-full h-[100vh] justify-center items-center flex-col gap-4">
+    <div className="flex w-full h-[100vh] justify-center items-center flex-col gap-6">
       <span className="text-7xl font-semibold"> Next App Stater Kit</span>
-      <span className="text-3xl text-gray-500"> Develop by Ali Zare</span>
+      <Link href="https://aliiz.ir/" className="text-3xl text-gray-500">
+        Develop by Ali Zare
+      </Link>
     </div>
   );
 }
